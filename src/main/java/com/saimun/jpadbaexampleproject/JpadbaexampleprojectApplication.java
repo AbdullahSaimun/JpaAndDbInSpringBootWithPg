5a9f999af59b0d656c37d46c5082a208d6b2e75f
Abdulla Al Saimun <44600426+AbdullahSaimun@users.noreply.github.com>
2024-05-06 06:58:25 +0600

    init

---
+++ src/main/java/com/saimun/jpadbaexampleproject/JpadbaexampleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.saimun.jpadbaexampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpadbaexampleprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpadbaexampleprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saimun/jpadbaexampleproject/JpadbaexampleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saimun.jpadbaexampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpadbaexampleprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
